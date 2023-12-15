7d7774b6ffd0dc2b7197b9dfd24b96f223261fcc
mohamedlrouabah1 <mohammed.rouabah@etu.univ-st-etienne.fr>
2023-12-15 12:10:16 +0100

    modified:   app/coopcycle.py

--- app/coopcycle.py
+++ app/coopcycle.py
@@ -1,19 +1,54 @@
+import json
 import requests
-from models import FoodItem
-from rdflib import Graph
+import requests
+from bs4 import BeautifulSoup
+import json
+import tqdm
 
-def collect_and_convert_data(url):
-    response = requests.get(url)
-    data = response.json()  # Supposons que la réponse est en JSON
+url = 'https://www.coopcycle.org/fr/'
 
-    graph = Graph()
-    for item in data:
-        food_item = FoodItem(item['name'], item['description'], item['price'], item['category'])
-        graph += food_item.to_rdf(graph)  # Ajouter à un graphe RDF
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+select_menu = soup.find('select', id='city-dropdown')
+# Extraire les valeurs (URLs) des options
+coop_urls = [option['value'] for option in select_menu.find_all('option') 
+            if option['value'] and option['value'].find('coopcycle.org') != -1]
+# Étape 2: Construire les sous-domaines
+coop_domains = [url for url in coop_urls]
+# discover urls of all subdomains
+coop_domains = [url + '/sitemap.xml' for url in coop_domains]
+# now get the JSON-LD from each list of each subdomain
+for domain in tqdm(coop_domains, desc='Scraping CoopCycle'):
+    response = requests.get(domain)
+    loc_tag = BeautifulSoup(response.content, 'html.parser').find_all('loc')
+    changefreq_tag = BeautifulSoup(response.content, 'html.parser').find_all('changefreq')
+    
+    for i in range(len(loc_tag)):
+        response = requests.get(loc_tag[i].text)
+        # print(loc_tag[i].text)
+        # print('----------------------------------------')
+        soup = BeautifulSoup(response.content, 'html.parser')
+        json_ld_script = soup.find('script', {'type': 'application/ld+json'})
+        if json_ld_script:
+            json_ld = json.loads(json_ld_script.string)
+            # print(json.dumps(json_ld, indent=4, sort_keys=True))
+            # print('----------------------------------------')
+# Fuseki server details
+fuseki_url = 'http://localhost:3030'  # Replace with your Fuseki server URL
+dataset_name = 'fooddataset'  # Replace with your dataset name in Fuseki
+# Endpoint for data insertion into Fuseki
+data_insertion_endpoint = f"{fuseki_url}/{dataset_name}/data"
+# Headers for sending JSON-LD
+headers = {
+    "Content-Type": "application/ld+json"
+}
+# Convert the JSON-LD data to a string
+json_dump = json.dumps(json_ld)
+# Send the JSON-LD data to Fuseki
+response = requests.post(data_insertion_endpoint, data=json_dump, headers=headers)
+# Check the response
+if response.status_code == 200:
+    print("Data successfully sent to Jena Fuseki.")
+else:
+    print(f"Failed to send data. Status code: {response.status_code}, Response: {response.text}")
 
-    return graph.serialize(format='turtle')  # Retourner le graphe RDF en format Turtle
-
-# Exemple d'utilisation
-url = "https://coopcycle.org/fr/federation/"
-rdf_data = collect_and_convert_data(url)
-# Envoyez ensuite ces données à Fuseki ici
